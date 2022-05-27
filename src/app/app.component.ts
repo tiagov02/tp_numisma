3680b0698c7e401db1e68f641a8ef70a583f23f3
Tiago Viana <tiagoviana.1003@gmail.com>
2022-05-27 16:42:50 +0100

    Comentei esta parte, quando acabarmos como já esta feito descomentamos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ export class AppComponent {
   public appPages = [
     { title: 'Página Principal', url: '/moedas', icon: 'home' },
     { title: 'Anunciar', url: '/formcoins', icon: 'trending-up' },
-    { title: 'Mensagens', url: '/registar', icon: 'chatbubbles' },
-    { title: 'Meus Anúncios', url: '/registar', icon: 'bag' },
-    { title: 'Perfil', url: '/registar', icon: 'person-circle' },
-    { title: 'Sair', url: '/registar', icon: 'log-out' }
+    //{ title: 'Mensagens', url: '/registar', icon: 'chatbubbles' },
+    //{ title: 'Meus Anúncios', url: '/registar', icon: 'bag' },
+    //{ title: 'Perfil', url: '/registar', icon: 'person-circle' },
+    //{ title: 'Sair', url: '/registar', icon: 'log-out' }
+    {title: 'Ver moedas',url:'/moedas',icon:'bag'}
+    //Comentei esta parte, quando acabarmos como já esta feito descomentamos
   ];
   constructor() {}
 }
